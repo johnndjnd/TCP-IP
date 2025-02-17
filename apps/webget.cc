53c7bff33ec923cfd64b14e2aa18ec5187cf19b8
johnndjnd <yizhongfu_idi@sjtu.edu.cn>
2025-02-17 11:26:42 +0800

    finish check0

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,20 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket socket {};
+  socket.connect( Address( host, "http" ) );
+  socket.write( "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n\r\n" );
+  socket.shutdown( SHUT_WR );
+  string response;
+  while ( !socket.eof() ) {
+    string chunk;
+    socket.read( chunk );
+    response += chunk;
+  }
+  socket.close();
+  cout << response;
+  //   cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
+  //   cerr << "Warning: get_URL() has not been implemented yet.\n";
 }
 
 int main( int argc, char* argv[] )
--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -6,51 +6,58 @@ ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
 void Writer::push( string data )
 {
-  (void)data; // Your code here.
+  if ( buffer_.size() + data.size() > capacity_ ) {
+    total_buffer_ += data.substr( 0, capacity_ - buffer_.size() ).size();
+    buffer_ += data.substr( 0, capacity_ - buffer_.size() );
+  } else {
+    total_buffer_ += data.size();
+    buffer_ += data;
+  }
 }
 
 void Writer::close()
 {
   // Your code here.
+  closed_ = true;
 }
 
 bool Writer::is_closed() const
 {
-  return {}; // Your code here.
+  return closed_; // Your code here.
 }
 
 uint64_t Writer::available_capacity() const
 {
-  return {}; // Your code here.
+  return capacity_ - buffer_.size(); // Your code here.
 }
 
 uint64_t Writer::bytes_pushed() const
 {
-  return {}; // Your code here.
+  return total_buffer_; // Your code here.
 }
 
 string_view Reader::peek() const
 {
-  return {}; // Your code here.
+  return buffer_; // Your code here.
 }
 
 void Reader::pop( uint64_t len )
 {
-  (void)len; // Your code here.
+  buffer_ = buffer_.substr( len, buffer_.size() - len ); // Your code here.
+  total_pop_ += len;
 }
 
 bool Reader::is_finished() const
 {
-  return {}; // Your code here.
+  return closed_ && buffer_.size() == 0; // Your code here.
 }
 
 uint64_t Reader::bytes_buffered() const
 {
-  return {}; // Your code here.
+  return buffer_.size(); // Your code here.
 }
 
 uint64_t Reader::bytes_popped() const
 {
-  return {}; // Your code here.
+  return total_pop_; // Your code here.
 }
-
